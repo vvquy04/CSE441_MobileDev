653aa4c4588109aab454ab4607c705f9847e5bb4
Văn Quý <vivanquy20804@gmail.com>
2025-04-26 14:54:23 +0700

    add new folders

---
+++ LabTutorials/app/src/main/java/com/example/demo_constrain/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.demo_constrain;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtA, edtB, edtKQ;
+    Button btntong;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btntong = findViewById(R.id.btntong);
+        btntong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int A = Integer.parseInt(edtA.getText().toString());
+                int B = Integer.parseInt(edtB.getText().toString());
+                int C = A + B;
+                edtKQ.setText(C + "");
+            }
+        });
+    }
+}
